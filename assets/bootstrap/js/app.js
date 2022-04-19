8316dbb5e14b6491008716395873281d74cdc214
Felix Odete <felix.odette@gmail.com>
2022-04-19 19:36:08 +0300

    Events section

--- assets/bootstrap/js/app.js
+++ assets/bootstrap/js/app.js
@@ -5,53 +5,69 @@ const app = [{
   image: 'assets/img/thumbnails/gallery_01_10_thumb.jpg',
   alt: "Alex's car",
   title: 'Alex',
-  text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
-},
-  {
-    cardClass: 'card',
-    image: 'assets/img/thumbnails/gallery_02_02_thumb.jpg',
-    alt: "John's car",
-    title: 'John',
-    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
+  text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+  cardClass1: 'card',
+  image1: 'assets/img/thumbnails/gallery_02_02_thumb.jpg',
+  alt1: "John's car",
+  title1: 'John',
+  text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
   },
   {
     cardClass: 'card',
     image: 'assets/img/thumbnails/gallery_01_13_thumb.jpg',
     alt: "Patrick's car",
     title: 'Patrick',
-    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
-  },
-  {
-    cardClass: 'card',
-    image: 'assets/img/thumbnails/gallery_01_10_thumb.jpg',
-    alt: "Alex's car",
-    title: 'Alex',
-    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
+    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+    cardClass1: 'card',
+    image1: 'assets/img/thumbnails/gallery_01_10_thumb.jpg',
+    alt1: "Alex's car",
+    title1: 'Alex',
+    text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
   },
   {
     cardClass: 'card',
     image: 'assets/img/thumbnails/gallery_02_02_thumb.jpg',
     alt: "John's car",
     title: 'John',
-    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
-  },
-  {
-    cardClass: 'card',
-    image: 'assets/img/thumbnails/gallery_01_13_thumb.jpg',
-    alt: "Patrick's car",
-    title: 'Patrick',
-    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
+    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+    cardClass1: 'card',
+    image1: 'assets/img/thumbnails/gallery_01_13_thumb.jpg',
+    alt1: "Patrick's car",
+    title1: 'Patrick',
+    text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
   }];
 
 function createCard(cardObject) {
   const div = document.createElement('div');
-  div.className = 'card';
-  div.innerHTML = `<img src="${cardObject.image}" alt="${cardObject.alt}" class="card-img-top w-100 d-block">
-    <div class="card-body">
-      <h4 class="card-title">${cardObject.title}</h4>
-      <p class="card-text">${cardObject.text}</p>
-    </div>
-  </div>`;
+  div.className = 'container';
+  // div.innerHTML = `<img src="${cardObject.image}" alt="${cardObject.alt}" class="card-img-top w-100 d-block">
+  //   <div class="card-body">
+  //     <h4 class="card-title">${cardObject.title}</h4>
+  //     <p class="card-text">${cardObject.text}</p>
+  //   </div>
+  // </div>`;
+  div.innerHTML = `<div class="row" id="card-rows">
+      <div class="col-md-6">
+          <div class="card">
+              <div class="card-body">
+                  <img id="card-img" src="${cardObject.image}" alt="${cardObject.alt}" />
+                  <h4 class="card-title">${cardObject.title}</h4>
+                  <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
+                  <p class="card-text">${cardObject.text}</p>
+              </div>
+          </div>
+      </div>
+      <div class="col-md-6">
+          <div class="card">
+              <div class="card-body">
+              <img id="card-img" src="${cardObject.image1}" alt="${cardObject.alt1} />
+                  <h4 class="card-title">${cardObject.title1}</h4>
+                  <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
+                  <p class="card-text">${cardObject.text1}</p>
+              </div>
+          </div>
+      </div>
+  </div>`
   return div;
 }
 
