7c97ef9707f6767fd77c9e2ad7940d2a688cff2a
Felix Odete <felix.odette@gmail.com>
2022-04-20 09:34:59 +0300

    Linter fixes

--- assets/bootstrap/js/app.js
+++ assets/bootstrap/js/app.js
@@ -12,36 +12,36 @@ const app = [{
   alt1: "John's car",
   title1: 'John',
   subtitle1: '2018 WRX',
-  text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
-  },
-  {
-    cardClass: 'card',
-    image: 'assets/img/thumbnails/gallery_01_13_thumb.jpg',
-    alt: "Patrick's car",
-    title: 'Patrick',
-    subtitle: '2017 WRX STI',
-    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
-    cardClass1: 'card',
-    image1: 'assets/img/thumbnails/gallery_01_10_thumb.jpg',
-    alt1: "Alex's car",
-    title1: 'Alex',
-    subtitle1: '2018 WRX',
-    text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
-  },
-  {
-    cardClass: 'card',
-    image: 'assets/img/thumbnails/gallery_02_02_thumb.jpg',
-    alt: "John's car",
-    title: 'John',
-    subtitle: '2019 WRX STI',
-    text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
-    cardClass1: 'card',
-    image1: 'assets/img/thumbnails/gallery_01_13_thumb.jpg',
-    alt1: "Patrick's car",
-    title1: 'Patrick',
-    subtitle1: '2018 WRX',
-    text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.'
-  }];
+  text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+},
+{
+  cardClass: 'card',
+  image: 'assets/img/thumbnails/gallery_01_13_thumb.jpg',
+  alt: "Patrick's car",
+  title: 'Patrick',
+  subtitle: '2017 WRX STI',
+  text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+  cardClass1: 'card',
+  image1: 'assets/img/thumbnails/gallery_01_10_thumb.jpg',
+  alt1: "Alex's car",
+  title1: 'Alex',
+  subtitle1: '2018 WRX',
+  text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+},
+{
+  cardClass: 'card',
+  image: 'assets/img/thumbnails/gallery_02_02_thumb.jpg',
+  alt: "John's car",
+  title: 'John',
+  subtitle: '2019 WRX STI',
+  text: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+  cardClass1: 'card',
+  image1: 'assets/img/thumbnails/gallery_01_13_thumb.jpg',
+  alt1: "Patrick's car",
+  title1: 'Patrick',
+  subtitle1: '2018 WRX',
+  text1: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+}];
 
 function createCard(cardObject) {
   const div = document.createElement('div');
@@ -67,7 +67,7 @@ function createCard(cardObject) {
               </div>
           </div>
       </div>
-  </div>`
+  </div>`;
   return div;
 }
 
